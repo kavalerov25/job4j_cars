cda16401e864f156cebc371b797b865826eb195c
kavalerov25 <paradise_777@mail.ru>
2022-12-08 17:55:19 +0300

    Модели и связи. Машины и владельцы [#4744 #368202] fix

---
+++ src/main/java/ru/job4j/cars/CarsApplication.java
@@ -0,0 +1,12 @@
+package ru.job4j.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CarsApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
