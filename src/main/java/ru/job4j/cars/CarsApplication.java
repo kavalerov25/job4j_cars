ba3a5ffcddb9db9e512c8906fe47218e770b333d
kavalerov25 <paradise_777@mail.ru>
2022-12-10 13:56:46 +0300

    Модели и связи. Машины и владельцы [#4744 #368202] fix2

--- src/main/java/ru/job4j/cars/CarsApplication.java
+++ src/main/java/ru/job4j/cars/CarsApplication.java
@@ -3,10 +3,13 @@ package ru.job4j.cars;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CarsApplication {
 
     public static void main(String[] args) {
 
     }
-
 }
--- src/main/java/ru/job4j/cars/repository/PostRepository.java
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -3,6 +3,7 @@ package ru.job4j.cars.repository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.cars.model.Post;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -10,22 +11,30 @@ import java.util.Map;
 @Repository
 @AllArgsConstructor
 public class PostRepository {
+    private static final String FIND_CAR_FOR_CURRENT_TIME =
+            """
+                    FROM Post WHERE created BETWEEN :fFrom AND :fTo
+            """;
     private final CrudRepository crudRepository;
 
     /**
      * показать объявления за последний день.
+     *
      * @return list of posts.
      */
     public List<Post> findCreatedLastDay() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
         return crudRepository.query(
-                "from Post where created > :fTime",
+                FIND_CAR_FOR_CURRENT_TIME,
                 Post.class,
-                Map.of("fTime", LocalDateTime.now().minusDays(1)));
+                Map.of("fTo", now, "fFROM", yesterday));
     }
 
 
     /**
      * показать объявления с фото.
+     *
      * @return list of posts.
      */
     public List<Post> findAllWithPhoto() {
@@ -36,6 +45,7 @@ public class PostRepository {
 
     /**
      * показать объявления определенной марки.
+     *
      * @param name Car brand
      * @return list of posts.
      */
